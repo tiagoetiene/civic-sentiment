4f2ffa811d32e61f8b44ede7d95bda294d209954
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2014-06-09 22:41:52 -0700

    Bringing plot back, just for testing.

--- civic-sentiment.js
+++ civic-sentiment.js
@@ -68,46 +68,49 @@ if (Meteor.isClient) {
     return this.candidate_image;
   }
 
-  // var size_0 = candidate_0.output.length;
-  // for(var repeat = 0; repeat < 100; ++repeat)
-  //   for(var i = 0; i < size_0; ++i)
-  //     candidate_0.output.push(candidate_0.output[i]);
+  function tmp_plot() {
+    var size_0 = candidate_0.output.length;
+    for(var repeat = 0; repeat < 100; ++repeat)
+      for(var i = 0; i < size_0; ++i)
+        candidate_0.output.push(candidate_0.output[i]);
 
-  // var size_1 = candidate_1.output.length;
-  // for(var repeat = 0; repeat < 100; ++repeat)
-  //   for(var i = 0; i < size_1; ++i)
-  //     candidate_1.output.push(candidate_1.output[i]);
+    var size_1 = candidate_1.output.length;
+    for(var repeat = 0; repeat < 100; ++repeat)
+      for(var i = 0; i < size_1; ++i)
+        candidate_1.output.push(candidate_1.output[i]);
 
-  // console.log(candidate_0);
-  // console.log(candidate_1);
+    console.log(candidate_0);
+    console.log(candidate_1);
 
-  // var data_0 = [];
-  // var data_1 = [];
+    var data_0 = [];
+    var data_1 = [];
 
-  // var idx_0 = 1;
-  // var idx_1 = 1;
+    var idx_0 = 1;
+    var idx_1 = 1;
 
-  // var plot = Plot();
+    var plot = Plot();
 
-  // function generateData() {
-  //   var jeanne = candidate_0.output;
-  //   var scott = candidate_1.output;
+    function generateData() {
+      var jeanne = candidate_0.output;
+      var scott = candidate_1.output;
 
-  //   for(var i =data_0.length; i < Math.min(jeanne.length,idx_0); ++i)
-  //     data_0.push( {sentiment : jeanne[i].score, date : new Date() } );
-  //   idx_0 += 1;
+      for(var i =data_0.length; i < Math.min(jeanne.length,idx_0); ++i)
+        data_0.push( {sentiment : jeanne[i].score, date : new Date() } );
+      idx_0 += 1;
 
-  //   for(var i =data_1.length; i < Math.min(scott.length,idx_0); ++i)
-  //     data_1.push( {sentiment : scott[i].score, date : new Date() } );
-  //   idx_1 += 1;
+      for(var i =data_1.length; i < Math.min(scott.length,idx_0); ++i)
+        data_1.push( {sentiment : scott[i].score, date : new Date() } );
+      idx_1 += 1;
 
-  //   var plot_div = d3.select('#plot');
-  //   plot_div.data( [ [ data_0, data_1] ] )
+      var plot_div = d3.select('#plot');
+      plot_div.data( [ [ data_0, data_1] ] )
 
-  //   plot(plot_div);
-  // }
+      plot(plot_div);
+    }
 
-  // setInterval(generateData, 1000)
+    setInterval(generateData, 1000)
+  }
+  tmp_plot();
 
 }
 
