c93d58692b0a195ca786352440150760fb72a57b
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2014-07-01 00:32:11 -0700

    Added url_feed to the list of candidates.
    url_feed field is used to populate our DB with tweets from this feed.

--- civic-sentiment.js
+++ civic-sentiment.js
@@ -4,56 +4,64 @@ AllCandidates = [
     party : 'Democratic',
     image : "resource/500px-Jeanne_Shaheen,_official_Senate_photo_portrait,_2009.jpg",
     iframe_src : "//rjihacks.wayinhub.com/cv-page-jeanne-shaheen?hidenav=true",
-    iframe_id : "1a0910b2-9765-4caf-8ae0-233ffda2fe1d"
+    iframe_id : "1a0910b2-9765-4caf-8ae0-233ffda2fe1d",
+    url_feed : "http://rjihacks.wayinhub.com/api/1/bites?feed=fd-5n1q39ji7ffcro4xhl&limit=200&format=json"
   },
   {
     name : "Scott Brown",
     party : 'Republican',
     image : "resource/500px-Sbrownofficial.jpg",
     iframe_src : "//rjihacks.wayinhub.com/cv-page-scott-brown?hidenav=true",
-    iframe_id : "b556773a-8f12-4213-ac41-a14a0219ccec"
+    iframe_id : "b556773a-8f12-4213-ac41-a14a0219ccec",
+    url_feed : "http://rjihacks.wayinhub.com/api/1/bites?feed=fd-2i8rllbn4nswdwznr6c&limit=200&format=json"
   },
   {
     name : "Barack Obama",
     party : "Democratic",
     image : "resource/500px-President_Barack_Obama.jpg",
     iframe_src : "//rjihacks.wayinhub.com/cv-page-obama?hidenav=true",
-    iframe_id : "2e0c206f-9dee-4edb-aafb-f9d71d12c1dc"
+    iframe_id : "2e0c206f-9dee-4edb-aafb-f9d71d12c1dc",
+    url_feed : "http://rjihacks.wayinhub.com/api/1/bites?feed=fd-2ia8kfr7v18vl20p3mu&limit=200&format=json"
   },
   {
     name : "Dilma Rousseff",
     party : "Partido dos Trabalhadores",
     image : "resource/500px-Dilma_Rousseff.jpg",
     iframe_src : "//rjihacks.wayinhub.com/cv-page-dilma-rousseff?hidenav=true",
-    iframe_id : "63ac1d56-e8fe-4a70-8dfd-f344e7fc2fc7"
+    iframe_id : "63ac1d56-e8fe-4a70-8dfd-f344e7fc2fc7",
+    url_feed : "http://rjihacks.wayinhub.com/api/1/bites?feed=fd-2ia98a68acnyqj01ur9&limit=200&format=json"
   },
   {
     name : "Eric Cantor",
     party : "Republican",
     image : "resource/500px-Eric_Cantor,_official_113th_Congress_photo_portrait.jpg",
     iframe_src : "//rjihacks.wayinhub.com/cv-page-eric-cantor?hidenav=true",
-    iframe_id : "eb7564ba-4e32-46b9-b30f-eef5a85f6727"
+    iframe_id : "eb7564ba-4e32-46b9-b30f-eef5a85f6727",
+    url_feed : "http://rjihacks.wayinhub.com/api/1/bites?feed=fd-2ib273ddr294xe32ije&limit=200&format=json"
   },
   {
     name : "Gerard Beloin",
     party : "Republican",
     image : ".",
     iframe_src : "//rjihacks.wayinhub.com/cv-page-gerard-beloin?hidenav=true",
-    iframe_id : "0c80f9f3-bb50-4534-a7d7-7217b6a5443d"
+    iframe_id : "0c80f9f3-bb50-4534-a7d7-7217b6a5443d",
+    url_feed : "http://rjihacks.wayinhub.com/api/1/bites?feed=fd-2ib6v707v4ibp35o62c&limit=200&format=json"
   },
   {
     name : "Robert D'Arcy",
     party : "Republican",
     image : ".",
     iframe_src : "//rjihacks.wayinhub.com/cv-page-robert-darcy?hidenav=true",
-    iframe_id : "a22a7b4c-5125-4254-8c07-49ac1549d774"
+    iframe_id : "a22a7b4c-5125-4254-8c07-49ac1549d774",
+    url_feed : "http://rjihacks.wayinhub.com/api/1/bites?feed=fd-2ib6y7ku6bmge2ne9oe&limit=200&format=json"
   },
   {
     name : "Bob Smith",
     party : "Republican",
     image : "http://bobsmithforussenate.com/wp-content/gallery/march-4-campaign-kickoff/003.png",
     iframe_src : "//rjihacks.wayinhub.com/cv-page-bob-smith?hidenav=true",
-    iframe_id : "e41192a2-070c-456f-b9b8-8c18ead29d20"
+    iframe_id : "e41192a2-070c-456f-b9b8-8c18ead29d20",
+    url_feed : "http://rjihacks.wayinhub.com/api/1/bites?feed=fd-2ib7730wfg3ghbr8srq&limit=200&format=json"
   },
 ]
 
