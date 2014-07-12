11433e92bb00ee2252ef6b39e17a7fc0c59e2120
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2014-07-11 17:21:04 -0700

    No longer using multiple databases

--- civic-sentiment.js
+++ civic-sentiment.js
@@ -1,20 +1,4 @@
 TwitterDB = new Meteor.Collection("tweets")
-WeekDB = Slice( "weeks" ).interval( millisecondsInAWeek / 30 )
-
-
-function setCandidateCursors(past) {
-	_.each(AllCandidates, function( _ , idx ) {
-    		var query_param = { date : { $gt : past },  name : AllCandidates[idx].name };
-    		AllCandidates[idx].cursor = TwitterDB.find(query_param);
-    		AllCandidates[idx].weekCursor = WeekDB.find( query_param );
-  	});
-}
-
-function setCandidateTwitterData() {
-	_.each(AllCandidates, function( _ , idx ) {
-		AllCandidates[idx].tweets = TwitterData(  );
-	});
-}
 
 if (Meteor.isClient) {
 	var past = -7 * 24 * 60 * 60 * 1000;
@@ -89,73 +73,30 @@ if (Meteor.isClient) {
 
 	var plot = Plot();
   	  	
-  	setCandidateCursors(getPast(new Date(), past));
-  	setCandidateTwitterData();
-
-  	_.each(AllCandidates, function( _ , idx) {
-  		AllCandidates[idx].cursor.observeChanges( {
-			added : function(id, obj) {
-	  			AllCandidates[idx].tweets.add( obj ); 
-	  		}
-	  	});
-  	});
-
 	function plotData() {
 		var data = [];
 		var _now = new Date();
 		var _past = getPast( _now, past );
 		var domain = [ _past, _now ];
 		var bins = 30;
-	  
+		var time_interval = Math.floor( Math.abs( past / bins ) );
+
 		_.each(SelectedCandidates, function(candidate) { 
-			var datum = candidate.tweets.data( _past );
+			Meteor.call('data', candidate.name, time_interval, _past, function(error, ret) {
+				
+				var max = d3.max( ret, function(d) { return Math.abs(d.sentiment) } );
+				if( max != 0 ) _.each( ret, function(d, idx) { ret[ idx ].sentiment /= max } );
+				
+				data.push( ret );
 
-			console.log( candidate.tweets.all().length ); 
-		  
-			// BUILDING HISTOGRAM
-			var time_interval = Math.abs( past / 1000 );
-			var bins = 30;
-
-			while(time_interval % bins != 0)
-				bins++;
-
-			var delta  = time_interval / bins; 
-			var interval_size = delta * 1000;
-			var interval_start = new Date( _past );
-
-			interval_start.setDate( interval_start.getDate() - 7 );
-			interval_start.setHours( 0 );
-			interval_start.setMinutes( 0 );
-			interval_start.setSeconds( 0 );
-			interval_start.setMilliseconds( 0 );
-
-			var ref_idx = Math.floor( (_past.valueOf() - interval_start.valueOf()) / interval_size );
-
-			var histogram = [];
-			for(var i = 0; i < bins ; ++i)
-				histogram.push({  
-					x : new Date(  interval_start.valueOf() + (i + ref_idx) * interval_size ), 
-					sentiment : 0.0 
-				});
-
-			_.each(datum, function(d) {
-				var idx = Math.floor( (d.date.valueOf() - interval_start.valueOf()) / interval_size );
-				idx -= ref_idx;
-				if(idx >= 0 && idx < histogram.length)
-					histogram[idx].sentiment += d.sentiment;
+				var plot_div = d3.select('#plot');
+				plot_div.data( [ data ] );
+				plot.domain( [  _past , _now ]  )
+					.x( function(d) { return d.date; } )
+					.y( function(d) { return d.sentiment; } )
+					(plot_div);
 			});
-
-			var max = d3.max( histogram, function(d) { return Math.abs(d.sentiment); } );
-			if(max != 0) _.each(histogram, function(d, idx) { histogram[idx].sentiment /= max } );
-			data.push( histogram );
 		});
-	      
-		var plot_div = d3.select('#plot');
-		plot_div.data( [ data ] );
-		plot.domain( [  _past , _now ]  )
-			.x( function(d) { return d.x; } )
-			.y( function(d) { return d.sentiment; } )
-			(plot_div);
 	}
 
 	setInterval(plotData, 1000);
