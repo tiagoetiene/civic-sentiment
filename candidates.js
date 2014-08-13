4c9f5b91804bcb0f728dc7aae47bca51c8208a2c
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2014-08-12 21:01:20 -0700

    Updated list of selected users

--- candidates.js
+++ candidates.js
@@ -5,7 +5,8 @@ AllCandidates = [
     image : "resource/500px-Jeanne_Shaheen,_official_Senate_photo_portrait,_2009.jpg",
     iframe_src : "//rjihacks.wayinhub.com/cv-page-jeanne-shaheen?hidenav=true",
     iframe_id : "1a0910b2-9765-4caf-8ae0-233ffda2fe1d",
-    color : '#2980B9'
+    color : '#2980B9',
+    id : 'Shaheen',
   },
   { 
     name : "Scott Brown",
@@ -13,7 +14,8 @@ AllCandidates = [
     image : "resource/500px-Sbrownofficial.jpg",
     iframe_src : "//rjihacks.wayinhub.com/cv-page-scott-brown?hidenav=true",
     iframe_id : "b556773a-8f12-4213-ac41-a14a0219ccec",
-    color : '#C0392B'
+    color : '#C0392B',
+    id : 'Brown',
   },
   {
     name : "Barack Obama",
@@ -21,7 +23,8 @@ AllCandidates = [
     image : "resource/500px-President_Barack_Obama.jpg",
     iframe_src : "//rjihacks.wayinhub.com/cv-page-obama?hidenav=true",
     iframe_id : "2e0c206f-9dee-4edb-aafb-f9d71d12c1dc",
-    color : '#2980B9'
+    color : '#2980B9',
+    id : 'Obama',
   },
   {
     name : "Dilma Rousseff",
@@ -29,7 +32,8 @@ AllCandidates = [
     image : "resource/500px-Dilma_Rousseff.jpg",
     iframe_src : "//rjihacks.wayinhub.com/cv-page-dilma-rousseff?hidenav=true",
     iframe_id : "63ac1d56-e8fe-4a70-8dfd-f344e7fc2fc7",
-    color : 'darkred'
+    color : 'darkred',
+    id : 'Rousseff',
   },
   {
 	name : 'Aécio Neves',
@@ -37,56 +41,9 @@ AllCandidates = [
 	image : 'http://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Aécio_Neves_em_junho_de_2013_-_2.jpg/320px-Aécio_Neves_em_junho_de_2013_-_2.jpg',
 	iframe_src : "//rjihacks.wayinhub.com/cv-page-aecio-neves?hidenav=true",
 	iframe_id : "4edd4db7-6b61-4100-baaf-881a065ad10d",
-	color : '#2980B9'
+	color : '#2980B9',
+    id : 'Neves',
   }
-  // {
-  //   name : "Eric Cantor",
-  //   party : "Republican",
-  //   image : "resource/500px-Eric_Cantor,_official_113th_Congress_photo_portrait.jpg",
-  //   iframe_src : "//rjihacks.wayinhub.com/cv-page-eric-cantor?hidenav=true",
-  //   iframe_id : "eb7564ba-4e32-46b9-b30f-eef5a85f6727",
-  //   url_feed : "http://rjihacks.wayinhub.com/api/1/bites?feed=fd-2ib273ddr294xe32ije&limit=200&format=json"
-  // },
-  // {
-  //   name : "Gerard Beloin",
-  //   party : "Republican",
-  //   image : ".",
-  //   iframe_src : "//rjihacks.wayinhub.com/cv-page-gerard-beloin?hidenav=true",
-  //   iframe_id : "0c80f9f3-bb50-4534-a7d7-7217b6a5443d",
-  //   url_feed : "http://rjihacks.wayinhub.com/api/1/bites?feed=fd-2ib6v707v4ibp35o62c&limit=200&format=json"
-  // },
-  // {
-  //   name : "Robert D'Arcy",
-  //   party : "Republican",
-  //   image : ".",
-  //   iframe_src : "//rjihacks.wayinhub.com/cv-page-robert-darcy?hidenav=true",
-  //   iframe_id : "a22a7b4c-5125-4254-8c07-49ac1549d774",
-  //   url_feed : "http://rjihacks.wayinhub.com/api/1/bites?feed=fd-2ib6y7ku6bmge2ne9oe&limit=200&format=json"
-  // },
-  // {
-  //   name : "Bob Smith",
-  //   party : "Republican",
-  //   image : "http://bobsmithforussenate.com/wp-content/gallery/march-4-campaign-kickoff/003.png",
-  //   iframe_src : "//rjihacks.wayinhub.com/cv-page-bob-smith?hidenav=true",
-  //   iframe_id : "e41192a2-070c-456f-b9b8-8c18ead29d20",
-  //   url_feed : "http://rjihacks.wayinhub.com/api/1/bites?feed=fd-2ib7730wfg3ghbr8srq&limit=200&format=json"
-  // },
-  // {
-  //   name : "Argentine",
-  //   party : "-",
-  //   image : "http://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/500px-Flag_of_Argentina.svg.png",
-  //   iframe_src : "//rjihacks.wayinhub.com/cv-page-arg?hidenav=true",
-  //   iframe_id : "735fdfdc-81ef-4aff-9315-573fd52e48a3",
-  //   url_feed : "http://rjihacks.wayinhub.com/api/1/bites?feed=fd-5nh01dn0n1rkvrjr0f&limit=200&format=json"
-  // },
-  // {
-  //   name : "Brazil",
-  //   party : "-",
-  //   image : "http://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/500px-Flag_of_Brazil.svg.png",
-  //   iframe_src : "//rjihacks.wayinhub.com/cv-page-brazil?hidenav=true",
-  //   iframe_id : "b3b04e65-966a-4386-815c-563d4a58f493",
-  //   url_feed : "http://rjihacks.wayinhub.com/api/1/bites?feed=fd-2ifd6uw7xhap0cwn8vo&limit=200&format=json"
-  // }, 
 ]
 
 SelectedCandidates = []
