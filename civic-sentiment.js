84cb4149b5866fd5dccd04ae387dae5d6caa269a
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2014-08-13 21:18:29 -0700

    Plotting one curve per candidate

--- civic-sentiment.js
+++ civic-sentiment.js
@@ -1,37 +1,38 @@
 TwitterCollection = new Meteor.Collection("tweets-summary")
+Politicians = People();
 
 if (Meteor.isClient) {
 	configInternationalization();
 	document.title =i18n('title');
-
-	var plot_colors = [];
-
-	for(var i = 0; i < AllCandidates.length; ++i)
-		AllCandidates[i].tweets_count = 0;
-
 	var past = -31 * 24 * 60 * 60 * 1000;
-
 	$(document).ready(function() { 
 		$("#e1").select({placeholder: i18n("selectPolitician")})
 			.on("change", function(e) {
-				var found = false; 
-				for(var i = 0; i < SelectedCandidates.length; ++i) {
-					console.log(e.val);
-					if($(this).val().indexOf(SelectedCandidates[i].name) != -1)
-						found = true;
-				}
-				if(found == true) 
-					return
-				for(var i = 0; i < AllCandidates.length; ++i) {
-					if($(this).val().indexOf(AllCandidates[i].name) != -1) {
-						SelectedCandidates.push(AllCandidates[i]);
-						Session.set('ListOfCandidates', !(Session.get('ListOfCandidates') == true) );
-						return
+				var selected = $(this).val();
+				for(var i = 0; i < Politicians.size(); ++i) {
+					Politicians( i ).selected = false;
+					for(var j = 0; j < selected.length; ++j) {
+						if( Politicians( i ).name.indexOf(selected[j]) != -1 ) {
+							Politicians( i ).selected = true;
+							break;
+						}
 					}
 				}
+				Session.set('ListOfCandidates', !(Session.get('ListOfCandidates') == true) );				
 				retrieveData();
 			});
 
+		var html = '';
+		_.each(Politicians.types(), function( type ) {
+			console.log( type, i18n( type ) );
+			html += '<optgroup label=' + i18n( type ) + '>';
+			_.each(Politicians(), function(politician) {
+				if(politician.type.indexOf(type) !== -1)
+					html += '<option>' + politician.name + '</option>';
+			});
+			html += '</optgroup>';
+		});
+		$("#e1").append( html ).selectpicker('refresh');
 		$("#pastMonth").change( function(e)	{ past = -31 * 24 * 60 * 60 * 1000; retrieveData(); refreshingTime = 28800000; });
 		$("#pastWeek").change( function(e)	{ past = - 7 * 24 * 60 * 60 * 1000; retrieveData(); refreshingTime = 3600000; });
 		$("#past3Day").change( function(e)		{ past = - 3 * 24 * 60 * 60 * 1000; retrieveData(); refreshingTime = 6000000; });
@@ -58,6 +59,11 @@ if (Meteor.isClient) {
 		return this.iframe_id;
 	}
 
+	Template.search.searchVisibility = function() {
+		Session.get('ListOfCandidates');
+		return (Politicians.hasSelected()) ? "visible" : "hidden";
+	}
+
 	Template.candidate_name.name = function() {
 		return this.name;
 	}
@@ -74,7 +80,8 @@ if (Meteor.isClient) {
 
 	Template.main.list_of_candidates = function() {
 		Session.get('ListOfCandidates')
-		return SelectedCandidates;
+		return Politicians();
+	}
 
 	Template.main.isCandidateVisible = function( a, b ) {
 		return (Politicians.visible( this.name )) ? "" : "hidden";
@@ -128,33 +135,32 @@ if (Meteor.isClient) {
 		var time_interval = Math.floor( Math.abs( past / bins ) );
 		var pair = getIndex( time_interval );
 
-		for(var i = 0; i < AllCandidates.length; ++i) {
-			var found = false;
-			for(var j = 0; j < SelectedCandidates.length; ++j)
-				if(AllCandidates[i].name === SelectedCandidates[j].name)
-					found = true
-			if(found == false)
-				data[ AllCandidates[ i ].name ] = undefined;
-		}
+		for(var i = 0; i < Politicians.size(); ++i)
+			if(Politicians( i ).selected !== true)
+				data[ Politicians( i ).name ] = undefined;
 		
-		_.each( SelectedCandidates, function(candidate, idx) { 
-			var query = { name : candidate.name, depth : pair.depth };
-			var r = TwitterCollection.findOne( query );
-			var ret = _.filter(r.data, function(d) {  return +d.date >= +_past; });
-						
-			SelectedCandidates[ idx ].tweets_count = 0;
-			_.each(ret, function(d) { SelectedCandidates[ idx ].tweets_count += d.counter; });
+		_.each( Politicians(), function(candidate, idx) { 
+			if(candidate.selected === true) {
+				var query = { name : candidate.name, depth : pair.depth };
+				var r = TwitterCollection.findOne( query );
+				if(r !== undefined) {
+					var ret = _.filter(r.data, function(d) {  return +d.date >= +_past; });
+								
+					Politicians( idx ).tweets_count = 0;
+					_.each(ret, function(d) { Politicians( idx ).tweets_count += d.counter; });
 
-			Session.set( candidate.name, SelectedCandidates[ idx ].tweets_count );
-			setTimeout( function() { Session.set(candidate.name+':color', 'color:black'); }, 3000);
+					Session.set( candidate.name, Politicians( idx ).tweets_count );
+					setTimeout( function() { Session.set(candidate.name+':color', 'color:black'); }, 3000);
 
-			var max = d3.max( ret, function(d) { return d.positive_sentiment } );
-			max = Math.max(max, d3.max( ret, function(d) { return Math.abs(d.negative_sentiment)} ));
-			if( max != 0 ) _.each( ret, function(d, idx) { ret[ idx ].positive_sentiment /= max } );
-			if( max != 0 ) _.each( ret, function(d, idx) { ret[ idx ].negative_sentiment /= max } );
-			if( max != 0 ) _.each( ret, function(d, idx) { ret[ idx ].sentiment /= max } );
+					var max = d3.max( ret, function(d) { return d.positive_sentiment } );
+					max = Math.max(max, d3.max( ret, function(d) { return Math.abs(d.negative_sentiment)} ));
+					if( max != 0 ) _.each( ret, function(d, idx) { ret[ idx ].positive_sentiment /= max } );
+					if( max != 0 ) _.each( ret, function(d, idx) { ret[ idx ].negative_sentiment /= max } );
+					if( max != 0 ) _.each( ret, function(d, idx) { ret[ idx ].sentiment /= max } );
 
-			SelectedCandidates[ idx ].data = ret;
+					Politicians( idx ).data = ret;
+				}
+			}
 		});
 	}
 
@@ -167,7 +173,7 @@ if (Meteor.isClient) {
 		var _past = getPast( _now, past );
 		var domain = [ _past, _now ];
 
-		_.each(SelectedCandidates, function(d) {
+		_.each(Politicians.selected(), function(d) {
 			var plot_div = d3.select( "#"+ d.id );
 			plot_div.data( [ d.data ] );
 			d.plot.domain( [  _past , _now ]  )
@@ -193,10 +199,6 @@ if (Meteor.isClient) {
 			retrievedDataId = setInterval(retrieveData, refreshingTime);
 		}
 	}, 500);
-
-	// Hack to fix image size. I've been trying to get the Wayin widget to be shaped as a perfect square
-	// but I did not succeed. Thus, this hack will solve the problem.
-    setInterval(function() { Session.set('UpdateImageHeight', !(Session.get('UpdateImageHeight') == true)); }, 1000);
 }
 
 if (Meteor.isServer) { Meteor.startup(function () { }); }
