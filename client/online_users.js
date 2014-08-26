62a758cc7faab700a5181b249db03f88c8f705fb
Tiago Etiene <tiago.etiene@gmail.com>
2014-08-26 16:31:20 -0700

    Trying user count again

--- client/online_users.js
+++ client/online_users.js
@@ -1,10 +1,10 @@
-// Template.onlineUsers.numberOfOnlineUsers = function() {
-// 	var user_count = Session.get('numberOfUsers');
-// 	if(user_count === 0)
-// 		return '';
-// 	return user_count + ' ' + i18n('users') + ' ' + i18n('online');
-// };
+Template.onlineUsers.numberOfOnlineUsers = function() {
+	var user_count = Session.get('numberOfUsers');
+	if(user_count === 0 || user_count === undefined)
+		return '';
+	return user_count + ' ' + i18n('users') + ' ' + i18n('online');
+};
 
-// setInterval(function() {
-// 	Session.set('numberOfUsers', Meteor.users.find().count());
-// }, 5000);
+setInterval(function() {
+	Session.set('numberOfUsers', Meteor.users.find().count());
+}, 5000);
