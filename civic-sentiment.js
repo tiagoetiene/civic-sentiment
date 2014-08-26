d0f2ee07d49d828f18d796110ab62cccf3b2f6cf
Tiago Etiene <tiago.etiene@gmail.com>
2014-08-26 14:21:02 -0700

    Instead of "undefined" we now show "loading"

--- civic-sentiment.js
+++ civic-sentiment.js
@@ -58,6 +58,8 @@ if ( Meteor.isClient ) {
 	Template.candidate_name.tweets_count = function() {
 		Session.get(this.name);
 		Session.set(this.name+':color', 'color:red');
+		if(this.tweets_count === undefined)
+			return i18n('Loading');
 		return this.tweets_count + ' ';
 	}
 
