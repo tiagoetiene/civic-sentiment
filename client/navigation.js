ac9e694026213af384c680af62f5ace5d4bd996a
Tiago Etiene <tiago.etiene@gmail.com>
2014-09-05 17:10:18 -0700

    Improved navigation bar with routers

--- client/navigation.js
+++ client/navigation.js
@@ -1,5 +1,10 @@
 Template.language.events( {
 	'click' : function(event) {
-		console.log(this, event);
+		document.title =i18n('title');
+		if( i18n.getLanguage() === 'pt-br' )
+			i18n.setLanguage('en-us');
+		else
+			i18n.setLanguage('pt-br');
+		Session.set( 'updateSelect', Session.get('updateSelect') !== true );
 	}
 });
