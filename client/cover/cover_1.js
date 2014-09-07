1bc3cf693602c0bc40c0fc94819c506a258407e6
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2014-09-06 18:20:19 -0700

    Internationalizing some of the images

--- client/cover/cover_1.js
+++ client/cover/cover_1.js
@@ -1,3 +1,7 @@
 Template.howToGetStarted.curveExplanation = function() {
 	return i18n('images.curveExplanation');
 }
+
+Template.howToGetStarted.selectBox = function() {
+	return i18n('images.selectBox');
+}
