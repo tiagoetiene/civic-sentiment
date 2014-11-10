27ffa50fc8784a1cdffd56dafa059be5576d138c
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2014-11-09 18:59:54 -0800

    Removed unnecessary code

--- client/comboboxObserver.js
+++ client/comboboxObserver.js
@@ -34,39 +34,8 @@ function updateRoute() {
 
 
 comboboxObserver = function() {
-
-	// Update route
 	Tracker.autorun( function() { updateRoute();  } );
-	Tracker.autorun( function() { updateCandidateOptionsCombobox(); } );
-
-	// Tracker.autorun( function() {
-	// 	updateSelector();
-	// 	updateAddressBar();
-	// });
-	
-	// updateTimeframeData();
-	// updateComboBox = Tracker.autorun( function () {
-	// 	$("#e1").selectpicker('val', urlParamSelected.get())
-	// 			.selectpicker('refresh')
-	// 			.selectpicker('render');
-	// });
-	// updateTimeSelector = Tracker.autorun( function () {
-	// 	var time = reactiveTimeframe.get();
-	// 	(time.indexOf('past month') !== -1) ? $('#past_month').addClass('active') : $('#past_month').removeClass('active');
-	// 	(time.indexOf('past week') !== -1) ? $('#past_week').addClass('active') : $('#past_week').removeClass('active');
-	// 	(time.indexOf('past 3 days') !== -1) ? $('#past_3_days').addClass('active') : $('#past_3_days').removeClass('active');
-	// 	(time.indexOf('past day') !== -1) ? $('#past_day').addClass('active') : $('#past_day').removeClass('active');
-	// 	(time.indexOf('past 8 hours') !== -1) ? $('#past_8_hours').addClass('active') : $('#past_8_hours').removeClass('active');
-	// 	(time.indexOf('past hour') !== -1) ? $('#past_hour').addClass('active') : $('#past_hour').removeClass('active');
-	// 	(time.indexOf('past 5 min') !== -1) ? $('#past_5_min').addClass('active') : $('#past_5_min').removeClass('active');
-	// });
-	// updateURL = Tracker.autorun( function () {	
-	// 	var goToURL = "/realtime?politicians=";
-	// 	_.each(urlParamSelected.get(), function(name) { goToURL += name + "," });
-	// 	goToURL += "&timeframe=" + reactiveTimeframe.get();
-	// 	Router.go(goToURL);
-	// });
-	
+	Tracker.autorun( function() { updateCandidateOptionsCombobox(); } );	
 	Tracker.autorun( function () { updateData(); } );
 }
 
