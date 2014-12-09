c4b9f687cf787ed89bdd7ceb986b043ed55c7bfa
Tiago Etiene Queiroz <tiago.etiene@gmail.com>
2014-12-08 23:05:08 -0800

    Many fixes

--- client/views/accountSelector/options.js
+++ client/views/accountSelector/options.js
@@ -1,41 +1,76 @@
+var reactivePoliticianSubscriptionHandler = new ReactiveVar( Meteor.subscribe( "accounts" ), _.isEqual );
+
 Template.OptionsT.helpers({
+	dataIsLoaded : function() {
+		return reactivePoliticianSubscriptionHandler.get().ready();
+	},
 	group : function() {
-		var options = { sort : {type : 1}, fields : { country : true } };
-		var countries = _.uniq(AccountsCollection
-			.find({}, options)
-			.fetch()
-			.map(function(d) { return d.country; }), true);
-		return countries;
+		var options = { sort : { "state" : 1 }, fields : { "state" : true } };
+		var politicians = AccountsCollection.find( {}, options ).fetch();
+		return _.uniq( politicians.map( function( d ) { return d["state"]; } ) );
 	},
 	label : function() {
-		return i18n('options.'+String(this));
+		var state = String(this);
+		if(_.isEmpty( state )) {
+			return "";
+		}
+		return i18n( "options." + String( this ) );
 	},
 	listOfPoliticians : function() {
-		var options = { sort : {name : 1}, fields : { name : true } };
-		return AccountsCollection.find({ country : String( this ) }, options).fetch();
+		var options = { sort : {"person/name" : 1}, fields : { "person/name" : true } };
+		return AccountsCollection.find( { "state" : String( this ) }, options).fetch();
 	}
 });
 
-Template.OptionsT.rendered = function() {
-	$("#e1").selectpicker('render').selectpicker('refresh');
-	$("#e1")
-		.on("change", function( e ) {
-			var names = []
-			_.each(e.target, function( t ) {
-				if( t.selected === true ) {
-					names.push( t.value );
-				}
-			});
-			reactiveUserSelectedNames.set( names );
-		});
+Template.OptionsT.created = function() {
+	Session.set( "politicianCount", 0 );
 }
 
+Template.OptionsT.rendered = function() {
+}
 
 Template.OptionT.rendered = function() {
-	$("#e1").selectpicker('render').selectpicker('refresh');
+	Session.set( "politicianCount" , Session.get( "politicianCount" ) + 1);
 }
 
+Template.OptionT.helpers({
+	partyIcon : function() {
+		var name = this["person/name"];
+		if( name.search(/\[D.*\]/ ) != -1 )
+			return "Dem";
+		else if( name.search(/\[R.*\]/ ) != -1 )
+			return "Rep";
+		return "";
+	}
+});
+
+Template.OptionT.helpers( {
+	name : function() {
+		return this["person/name"].replace(/ *\[.*\]/, "");
+	}
+});
+
 updateCandidateOptionsCombobox = function( ) {
+
+	Tracker.autorun( function() {
+		if( Session.get( "politicianCount" ) == AccountsCollection.find( {} ).count() ) {
+			$( "#e1" )
+				.selectpicker( "render" )
+				.selectpicker( "refresh" );
+			$( "#e1" )
+				.on( "change", function( e ) {
+					var names = []
+					_.each(e.target, function( t ) {
+						if( t.selected === true ) {
+							names.push( t.value );
+						}
+					});
+					reactiveUserSelectedNames.set( names );
+				});
+			Session.set( "politicianCount", 0 );
+		}
+	});
+
 	Meteor.setTimeout( function() {
 		var names = reactiveSelectedNames.get();
 		if( names.length ) {
